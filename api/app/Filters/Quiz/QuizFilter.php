ca5f7345a7705806295371c640dd8d01b72bdefa
Wafukho Kelly Joseph <kellyjoe256@gmail.com>
2020-07-13 13:33:15 +0300

    quiz filter for taking quiz

--- api/app/Filters/Quiz/QuizFilter.php
+++ api/app/Filters/Quiz/QuizFilter.php
@@ -3,6 +3,7 @@
 namespace App\Filters\Quiz;
 
 use App\Filters\AbstractModelFilter;
+use App\Filters\Quiz\Filters\TakeTestFilter;
 use App\Filters\Quiz\Filters\UserFilter;
 
 class QuizFilter extends AbstractModelFilter
@@ -10,5 +11,8 @@ class QuizFilter extends AbstractModelFilter
     protected $filters = [
         'user' => UserFilter::class,
         'user_id' => UserFilter::class,
+        'tt' => TakeTestFilter::class,
+        'take-test' => TakeTestFilter::class,
+        'take_test' => TakeTestFilter::class,
     ];
 }
