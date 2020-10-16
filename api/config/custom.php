acf880599352d5007b0ee08eb6f0f22f4f208b9c
Wafukho Kelly Joseph <kellyjoe256@gmail.com>
2020-10-16 13:03:42 +0300

    access cookie duration setting

--- api/config/custom.php
+++ api/config/custom.php
@@ -4,4 +4,7 @@ return [
     'per_page' => env('PER_PAGE', 10),
 
     'access_cookie_name' => env('ACCESS_COOKIE_NAME', 'access_token'),
+
+    // in minutes
+    'access_cookie_duration' => env('ACCESS_COOKIE_DURATION', 60),
 ];
